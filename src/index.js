a8d3da62e38a0d576a15d53c64adad80b175d5a5
blaiseludvig <blaiseludvig@gmail.com>
2022-10-12 10:27:07 +0200

    implement basic functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import "./style.css";
 // import { animateCSS } from "./animatecss";
 
-let element = document.createElement('h1');
-element.textContent = "Ha ezt látod, akkor működik!"
-document.body.append(element);
+let images = document.querySelectorAll("#food-grid > img");
+let food_list = document.querySelector("#food-list");
+let calories = document.querySelector("#calories");
+let reset = document.querySelector("#reset");
+
+images.forEach((img) => {
+  img.addEventListener("click", () => {
+    let li = document.createElement("li");
+    li.innerHTML = `${img.dataset.foodname} <i>(${img.dataset.calories})</i>`;
+
+    calories.textContent = parseInt(img.dataset.calories);
+
+    food_list.appendChild(li);
+
+  });
+});
+
+reset.addEventListener("click", () => {
+  food_list.innerHTML = "";
+})
